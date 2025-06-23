0dd7787fee42db1d10a4c9ccbe124381a167863b
Jakom <emmanuelomondiobare@gmail.com>
2025-06-23 16:33:44 +0300

    Update KidsHub.js

--- src/pages/KidsHub.js
+++ src/pages/KidsHub.js
@@ -1,1 +1,9 @@
+// src/pages/KidsHub.js
+import React from 'react';
+import PlaceholderPage from '../PlaceholderPage';
 
+function KidsHub() {
+  return <PlaceholderPage title="Kids Hub" />;
+}
+
+export default KidsHub;
